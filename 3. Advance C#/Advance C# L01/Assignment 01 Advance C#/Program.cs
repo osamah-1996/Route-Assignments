00e294bb7c44b421265f8b56f06b5056286ad1ff
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-26 19:03:58 +0300

    Advance C# L01 Submmition

---
+++ 3. Advance C#/Advance C# L01/Assignment 01 Advance C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_01_Advance_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
