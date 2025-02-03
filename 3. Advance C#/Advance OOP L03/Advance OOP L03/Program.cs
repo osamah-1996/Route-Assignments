4c86d108c76304289490f07c685a513ad906f98f
osamah-1996 <osamah.2018.1996@gmail.com>
2025-02-03 15:28:04 +0300

    Advance OOP L02

---
+++ 3. Advance C#/Advance OOP L03/Advance OOP L03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advance_OOP_L03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
