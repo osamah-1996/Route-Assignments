66bce8643ad9824baa4c0579fcc6b329b37ef0d6
osamah-1996 <osamah.2018.1996@gmail.com>
2025-02-12 08:11:53 +0300

    LinQ L01

---
+++ 5. C# LinQ/LinQ L01 Assignment/LinQ L01/Data/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_L01.Data
+{
+    internal class Customer
+    {
+        //properaties
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public Order[] Orders { get; set; }
+        //Constractour
+        public Customer(string customerID, string customerName)
+        {
+            CustomerID = customerID;
+            CustomerName = customerName;
+            Orders = new Order[10];
+        }
+
+        //Overrid tostring()
+        public override string ToString()
+            => $"{CustomerID}, {CustomerName}, {Address}, {City}, {Region}, {PostalCode}, {Country}, {Phone}, {Fax}";
+    }
+}
---
+++ 5. C# LinQ/LinQ L01 Assignment/LinQ L01/Data/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_L01.Data
+{
+    internal class Product
+    {
+        //Properaties
+        public long ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //Constactour
+        //public int CompareTo(Product? other)
+        //{
+        //    return this.UnitPrice.CompareTo(other?.UnitPrice);
+        //}
+
+        //Override to string
+        public override string ToString()
+            => $"ProductID:{ProductID},ProductName:{ProductName},Category{Category},UnitPrice:{UnitPrice},UnitsInStock:{UnitsInStock}";
+    }
+}
